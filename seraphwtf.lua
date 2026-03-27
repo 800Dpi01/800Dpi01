78bc0780687745686539bd59242fb274e649d045
800Dpi01 <ngohoaiphuoc25@gmail.com>
2026-03-27 13:16:27 +0700

    Update seraphwtf.lua

--- seraphwtf.lua
+++ seraphwtf.lua
@@ -428,20 +428,11 @@ local sfx = {
 	["terraria slime"] = "rbxassetid://6916371803",
 }
 for _, file in listfiles("seraph/sounds/") do
-    -- CÁCH 1: Dùng string.gsub an toàn hơn
-    local fileName = string.gsub(file, "seraph/sounds/", "")
-    fileName = string.gsub(fileName, "%.mp3$", "")
-    
-    if fileName ~= file then  -- Nếu có thay đổi (là file .mp3)
+    -- Lấy tên file từ đường dẫn
+    local fileName = file:match("([^/\\]+)%.mp3$")
+    if fileName then
         sfx[fileName] = getcustomasset(file)
     end
-    
-    -- HOẶC CÁCH 2: Kiểm tra trước khi dùng split
-    -- local parts = string.split(file, "sounds\\")
-    -- if parts[2] then
-    --     local fileName = parts[2]:gsub(".mp3", "")
-    --     sfx[fileName] = getcustomasset(file)
-    -- end
 end
 
 local vec2 = Vector2.new
