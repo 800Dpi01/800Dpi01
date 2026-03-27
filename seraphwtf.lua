6e7b882bcaf3a489ab43613089366e38ae053f2d
800Dpi01 <ngohoaiphuoc25@gmail.com>
2026-03-27 13:27:19 +0700

    Update seraphwtf.lua

--- seraphwtf.lua
+++ seraphwtf.lua
@@ -428,7 +428,7 @@ local sfx = {
 	["terraria slime"] = "rbxassetid://6916371803",
 }
 for _, file in listfiles("seraph/sounds/") do
-	local fileName = string.split(file, "sounds\\")[2]:gsub(".mp3", "")
+	local fileName = file:match("([^\\/]+)$"):gsub("%.mp3", "")
 	sfx[fileName] = getcustomasset(file)
 end
 
